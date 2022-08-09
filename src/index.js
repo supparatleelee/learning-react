fd6ef55a59b38b6045fb066cd5523d8e8e8d081b
supparatleelee <supparat.lst@gmail.com>
2022-08-09 16:44:41 +0700

    finised learning React components-props topic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Welcome(props) {
+  console.log('Welcome Component');
+  return (
+    <h1>
+      Hi {props.name}, {props.age}
+    </h1>
+  );
+}
+
+// class Position extends React.Component {
+class Position extends Component {
+  render() {
+    return <h1>Position Component</h1>;
+    //about how to use props in class component will learn later.
+  }
+}
+
+function MainContainer(props) {
+  const birthDate = '2001-08-16'; // parent component
+  console.logo(props.test);
+  console.logo(props.obj.name);
+  console.logo(props.boo); // true = boolean can exclude ={} in component case.
+  return (
+    <div>
+      {/* Child component */}
+      {/* unidirectional data flow: Parent component send data to Child component only */}
+      <Welcome name="Bob" age="55" birthDate={birthDate} />
+      <Position />
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    {/* <Position />
+    {Welcome({ name: 'John' })}
+    <Welcome name="Alex" age="20" />
+    <Welcome name="Harry" />
+    <Welcome name="Sam" />
+    {Welcome({ name: 'Alex' })}
+    {Welcome({ name: 'Harry' })}
+    {Welcome({ name: 'Sam' })}
+    {Welcome({ name: 'Peter' })} */}
+    <MainContainer
+      test={[1, 2, 3]}
+      obj={{ name: 'Alex', surname: 'John' }}
+      boo
+    />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
