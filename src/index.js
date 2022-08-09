ef43dd4e2249d8143964783961d28b7130049cd6
supparatleelee <supparat.lst@gmail.com>
2022-08-09 17:12:36 +0700

    finised components-props-lab

--- src/index.js
+++ src/index.js
@@ -4,11 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Lab #1
+/*
+function Profile(props) {
+  return (
+    <div>
+      <h1>
+        Name: {props.name}, Age: {props.age}
+      </h1>
+    </div>
+  );
+}
+
+<Profile name="John Doe" age="20" />
+*/
+
+// Lab #2
+/*
+Honda
+1000000
+undefined // not show // because no value
+*/
+
+// Lab #3 & #4
+/*
+function ProductItem(props) {
+  return (
+    <div>
+      <h1>Product Name: {props.item.product}</h1>
+      <h2>Price: {props.item.price}</h2>
+      <p>Description: {props.item.info}</p>
+    </div>
+  );
+}
+// Lab 3
+<ProductItem product="Pen" price="20" info="For Writing" />
+// Lab 4
+<ProductItem item={{ product: 'Pen', price: 20, info: 'For Writing' }} />
+*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ProductItem item={{ product: 'Pen', price: 20, info: 'For Writing' }} />
 );
 
 // If you want to start measuring performance in your app, pass a function
