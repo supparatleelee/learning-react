52c9c6585f9398892042c016c6ab7e31f9770946
supparatleelee <supparat.lst@gmail.com>
2022-08-10 15:59:23 +0700

    finished learning conditional-rendering topic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+  // if isLogged is true -> render welcome back
+  // Else -> render go to login page
+  // === render as we setting (condition)
+
+  //1. Store jsx in a variable
+  // const message = isLogged === true ? <h1>Welcome Back</h1> : <h1>Go to Login Page</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 1. */}
+      {/* {message} */}
+
+      {/* 2. */}
+      {/* {isLogged && <h1>Welcome Back</h1>}
+      {!isLogged && <h1>Go to Login Page</h1>} */}
+
+      {/* 3. Ternary Operator */}
+      {isLogged === true ? <h1>Welcome Back</h1> : <h1>Go to Login Page</h1>}
     </div>
   );
+
+  //2. && Operator (null && <h1>Test</h1> ===> Boolean -> falsy value - return the fist one (?) ===> null)
+  //(true && <h1>Test</h1> ===> <h1>Test</h1>)
 }
 
 export default App;
