e49f83d8fa9da8d576604721bfa148bc8016e9a9
supparatleelee <supparat.lst@gmail.com>
2022-08-11 10:08:34 +0700

    finished state-lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+const data = {
+  bangkok: ['Pathum Wan', 'Dusit', 'Ratchathewi'],
+  nonthaburi: ['Pakkret', 'Bangyai'],
+};
+
 function App() {
   // Lab 1
   // 0. because we need to change the component props
@@ -45,25 +50,34 @@ function App() {
   //   });
   // };
 
-  // Lab 5
-  // const [text, setText] = useState('');
+  ////////////////////// Instructor ////////////////////////////
+  // create another state to get input value
+  // const [input4, setInput4] = useState('')
 
+  // Lab 5
   // GET tel input to use it the state[0] to check it in other
   // const [tel, setTel] = useState('');
+  // // error text
+  // const [text, setText] = useState('');
 
   // const check = (event) => {
+  //   // setTel(''); // ??? why it doesn't disappear
   //   setTel(event.target.value);
   //   // console.log(tel);
   // };
 
-  // const onSubmit = (event) => {
+  // const onClick = (event) => {
   //   if (tel === '') {
   //     setText('Phone number is required');
-  //   } else if (typeof tel !== 'number') {
+  //   } else if (isNaN(tel) === true) {
+  //     // } else if (typeof tel !== 'number') {
   //     //???? why already num, but this still applied?
+  //     // I guess it is because this condition that affect the error
+  //     // convert to number is nan = Not a number
   //     setText('Phone number is invalid');
-  //   } else if (tel.split('').length !== 10) {
+  //     // } else if (tel.split('').length !== 10) {
   //     //???? why still can't get into this condition??
+  //   } else if (tel.length !== 10) {
   //     console.log(event.target.value);
   //     setText('Invalid length');
   //   } else {
@@ -72,6 +86,7 @@ function App() {
   // };
 
   // Lab 6
+  const [district, setDistrict] = useState([]);
 
   return (
     <div>
@@ -99,18 +114,52 @@ function App() {
         name="dollar"
         onChange={onChange}
       ></input>
-      <p>Convert to Bath: {convertBath} Bath</p> */}
+      <p>
+        Convert to Bath: <span style={{ color: 'red' }}>{convertBath}</span>{' '}
+        Bath
+      </p> */}
 
       {/* Lab 5 */}
       {/* <form onSubmit={onSubmit}> */}
       {/* <input type="tel" placeholder="Phone Number" onChange={check}></input>
-      <button onClick={onSubmit}>Check</button> */}
+      <button onClick={onClick}>Check</button> */}
       {/* </form> */}
       {/* cuz the {text} won't last long */}
-      {/* <br />
-      <p style={{ color: 'red' }}>{text}</p> */}
+      {/* <br /> */}
+      {/* <p style={{ color: 'red' }}>{text}</p> */}
+      {/* Not good because it will always render the empty p tag */}
+      {/* Conditional Rendering */}
+      {/* {text && <p style={{ color: 'red' }}>{text}</p>} */}
 
       {/* Lab 6 */}
+      <label htmlFor="province">Province: </label>
+      <select
+        id="provice"
+        onChange={(event) => {
+          console.log(event.target.value);
+          setDistrict(data[event.target.value]);
+        }}
+      >
+        {/* pass parameter into callback function */}
+        <option value="bangkok">Bangkok</option>
+        <option value="nonthaburi">Nonthaburi</option>
+      </select>
+
+      <br />
+
+      <label htmlFor="district">District: </label>
+      <select id="district">
+        {/* still no need to put option because district will depend on provice */}
+        {/* Need to create data mapping -- const data -- outside App() function*/}
+        {/* things that {expression} can render -> 'string', number, ['12', '45'] */}
+        {/* But it will render like a concat, not individual */}
+        {/* but object, can't & not even concat === {name: 'test'} <- can't do it like this */}
+        {/* so we need to convert it into array and make it concat as we wanted */}
+        {/* one way: {[<option></option>, <option></option>]} */}
+        {district.map((item) => (
+          <option>{item}</option>
+        ))}
+      </select>
     </div>
   );
 }
